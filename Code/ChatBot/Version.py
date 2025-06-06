7799b6087c61a3cba262559d89cc36f0976c6f56
Begolosamazaref <avabego123@gmail.com>
2025-06-06 22:59:02 +0300

    Update repository structure and README.md with demo link

---
+++ Code/ChatBot/Version.py
@@ -0,0 +1,13 @@
+import flask
+import transformers
+import torch
+import fuzzywuzzy
+import requests
+import bs4
+
+print("Flask version:", flask.__version__)
+print("Transformers version:", transformers.__version__)
+print("Torch version:", torch.__version__)
+print("FuzzyWuzzy version:", fuzzywuzzy.__version__)
+print("Requests version:", requests.__version__)
+print("BeautifulSoup version:", bs4.__version__)
